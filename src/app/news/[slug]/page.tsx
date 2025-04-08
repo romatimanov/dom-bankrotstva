bac1334fe92c966ec42ebddc2af45945e742791c
ROMAN DESHEVITSYN <romatimanov@MacBook-Pro-ROMAN.local>
2025-04-08 13:49:16 +0700

    change news

--- src/app/news/[slug]/page.tsx
+++ src/app/news/[slug]/page.tsx
@@ -3,13 +3,12 @@ import { Post } from 'app/components/article/post'
 import { Metadata } from 'next'
 import { pool } from 'app/lib/db'
 
-export async function generateMetadata({
-  params
-}: {
-  params: Promise<{ slug: string }>
-}): Promise<Metadata> {
-  const { slug } = await params
-  const decodedSlug = decodeURIComponent(slug)
+type PageProps = {
+  params: { slug: string }
+}
+
+export async function generateMetadata({ params }: PageProps): Promise<Metadata> {
+  const decodedSlug = decodeURIComponent(params.slug)
 
   const [rows] = await pool.query<Article[]>(
     'SELECT title, metakey, metadescription FROM articles WHERE slug = ?',
@@ -37,17 +36,10 @@ export async function generateMetadata({
   }
 }
 
-export default async function NewsPage({
-  params
-}: {
-  params: { slug: string } | Promise<{ slug: string }>
-}) {
-  const { slug } = await params
-  const decodedSlug = decodeURIComponent(slug)
+export default async function NewsPage({ params }: PageProps) {
+  const decodedSlug = decodeURIComponent(params.slug)
 
-  const db = pool
-
-  const [rows] = await db.query<Article[]>('SELECT * FROM articles WHERE slug = ?', [decodedSlug])
+  const [rows] = await pool.query<Article[]>('SELECT * FROM articles WHERE slug = ?', [decodedSlug])
 
   if (rows.length === 0) {
     return <h1>Статья не найдена по slug</h1>
@@ -55,9 +47,5 @@ export default async function NewsPage({
 
   const article = rows[0]
 
-  return (
-    <>
-      <Post article={article} />
-    </>
-  )
+  return <Post article={article} />
 }
